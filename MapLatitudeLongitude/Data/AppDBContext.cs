d5fb55fb9b98f9274cf85cb0534f049fafcced3e
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-06 15:28:35 +0530

    Add AppDBContext with Museums DbSet
    Introduces the AppDBContext class inheriting from DbContext, including a DbSet for Museum entities to enable database operations.

---
+++ MapLatitudeLongitude/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using MapLatitudeLongitude.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MapLatitudeLongitude.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        public DbSet<Museum> Museums { get; set; }
+    }
+}
