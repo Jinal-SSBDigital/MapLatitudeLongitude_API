cf0a942fa9e94c9cddf9c672f8da3fd4347e9014
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-06 15:28:27 +0530

    Add MuseumLocationDto model
    Introduces MuseumLocationDto class to represent museum location data with ID, name, latitude, and longitude properties.

---
+++ MapLatitudeLongitude/Model/MuseumLocationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MapLatitudeLongitude.Model
+{
+    public class MuseumLocationDto
+    {
+        public int MuseumId { get; set; }
+        public string MuseumName { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
