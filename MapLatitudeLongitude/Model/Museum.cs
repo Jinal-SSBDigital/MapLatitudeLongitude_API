a3c63730c5248211dda57a5dd47ecc62733aa0ce
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-06 15:27:54 +0530

    Add Museum model class
    Introduces the Museum entity with properties for identification, contact details, location (latitude and longitude), ticket pricing, and foreign keys for state and city. This model will be used to represent museums in the application.

---
+++ MapLatitudeLongitude/Model/Museum.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MapLatitudeLongitude.Model
+{
+    public class Museum
+    {
+        [Key]
+        public int Museum_id { get; set; }
+
+        public string Museum_Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Descr { get; set; }
+        public string Open_time { get; set; }
+        public string Close_time { get; set; }
+        public decimal Ticket_price { get; set; }
+        public decimal Virtual_price { get; set; }
+
+        public decimal? latitude { get; set; }
+        public decimal? longitude { get; set; }
+
+        public bool status { get; set; }
+        public int Fk_StateId { get; set; }
+        public int Fk_CityId { get; set; }
+    }
+}
