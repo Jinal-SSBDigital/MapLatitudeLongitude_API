3a6d12049f92bee320283bd07eac21cf0241a46e
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-01-17 16:45:29 +0530

    Add MuseumLocation API controller
    Introduces MuseumLocation controller with a GET endpoint to retrieve museum location details by museum ID. Returns location data as MuseumLocationDto or a not found response if the museum does not exist.

---
+++ MapLatitudeLongitude/Controllers/MuseumLocation.cs
@@ -0,0 +1,31 @@
+﻿using MapLatitudeLongitude.Data;
+using MapLatitudeLongitude.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MapLatitudeLongitude.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MuseumLocation : ControllerBase
+    {
+        private readonly AppDBContext _context;
+
+        public MuseumLocation(AppDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMuseumLocations(int MId)
+        {
+            var location = await _context.Museums.Where(m => m.Museum_id == MId)
+                .Select(m => new MuseumLocationDto { MuseumId = m.Museum_id, MuseumName = m.Museum_Name, Latitude = m.latitude, Longitude = m.longitude })
+                .FirstOrDefaultAsync();
+
+            if (location == null)
+                return NotFound("Museum not found");
+            return Ok(location);
+        }
+    }
+}
